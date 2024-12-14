a2e78a2f84c8571455d71a6d5d32b7f2af916ab0
fyx1t <fyx1t@proton.me>
2024-12-14 21:12:17 +0300

    Add necessary files

---
+++ modules/analyzer.py
@@ -0,0 +1,15 @@
+class Analyzer:
+    def __init__(self):
+        self.__load_paths()
+
+    def __load_paths(self):
+        pass
+
+    def identify_entry_points(self):
+        pass
+
+    def identify_fuzzers_pool(self) -> dict:
+        return {}
+
+if __name__ == '__main__':
+    pass
---
+++ modules/fuzzer.py
@@ -0,0 +1,10 @@
+class Fuzzer:
+    def __init__(self, pool):
+        self.pool = pool
+    
+    def fuzz():
+        pass
+
+
+if __name__ == '__main__':
+    pass
